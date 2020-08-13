8b18ab8456df631dac4d0f59b3504d701ae214d1
lizzethgd <lizzethgd@gmail.com>
2020-08-13 14:13:53 +0300

    delploy try 12

--- backend/app.js
+++ backend/app.js
@@ -20,10 +20,10 @@ app.use(cors())
 app.use('/api/users', require('./routes/users'))
 app.use('/api/notes', require('./routes/notes'))
 
-app.use(express.static(path.join(__dirname, "frontend", "build")))
+app.use(express.static(path.join(__dirname, '../build')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.join(__dirname, '../build/index.html'))
 })
 
 module.exports = app
